17d5f05da305cb26ac228b2f1ea04aa04a8f8b6e
甘斌 <ganbin@ganbindeMacBook-Pro.local>
2017-03-21 21:42:09 +0800

    222555

---
+++ TestGit1/TestGit1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit1
+//
+//  Created by 甘斌 on 2017/3/21.
+//  Copyright © 2017年 NNGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit1/TestGit1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit1
+//
+//  Created by 甘斌 on 2017/3/21.
+//  Copyright © 2017年 NNGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
